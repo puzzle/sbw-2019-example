042aebee1e3fdd12c55c883e627d88f2cca58e7a
Khôi Tran <kt@compile.ch>
2019-10-04 12:55:05 +0200

    added custom objectmapper

---
+++ src/main/java/ch/puzzle/spring/workshop/beerio/configuration/ObjectMapperConfiguration.java
@@ -0,0 +1,17 @@
+package ch.puzzle.spring.workshop.beerio.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+
+@Configuration
+public class ObjectMapperConfiguration {
+    @Bean
+    ObjectMapper objectMapper() {
+        return new Jackson2ObjectMapperBuilder()
+                .featuresToEnable(SerializationFeature.INDENT_OUTPUT)
+                .build();
+    }
+}
