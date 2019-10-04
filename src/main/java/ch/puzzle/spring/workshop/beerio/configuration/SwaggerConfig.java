5ee86851c33e94824df1680bec7abd06b3922e13
Khôi Tran <kt@compile.ch>
2019-10-04 12:36:12 +0200

    starter done.

--- src/main/java/ch/puzzle/spring/workshop/beerio/configuration/SwaggerConfig.java
+++ src/main/java/ch/puzzle/spring/workshop/beerio/configuration/SwaggerConfig.java
@@ -1,9 +1,13 @@
 package ch.puzzle.spring.workshop.beerio.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -11,12 +15,26 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @Configuration
 @EnableSwagger2
 public class SwaggerConfig {
+    @Autowired
+    BuildProperties buildProperties;
+
+
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("api")
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.ant("/api/**"))
                 .build();
     }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title(buildProperties.getArtifact())
+                .version(buildProperties.getVersion())
+                .build();
+    }
+
 }
